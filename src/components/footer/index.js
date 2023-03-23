1b73a8fb4d57b3d881435f2f6741b009994e21b6
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2023-03-23 13:50:56 +0530

    google fonts integrated and footer updated

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,16 +4,25 @@ const Footer = () => {
       <span>
         Developed By{" "}
         <a
-          className="underline focus-yellow"
+          className="underline focus-yellow mx-1"
           href="https://mdtarhini.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           MD. Tarhini
         </a>
+        Maintaining by{" "}
+        <a
+          className="underline focus-yellow mx-1"
+          href="https://abhidadhaniya.com?ref=cheatsheetmaker"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Abhi Dadhaniya
+        </a>
       </span>
       <a
-        href="https://github.com/mdtarhini/cheat-sheet-maker"
+        href="https://github.com/abhidadhaniya23/cheat-sheets-maker-client"
         className="underline focus-yellow"
         target="_blank"
         rel="noopener noreferrer"
