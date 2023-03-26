bb5652e30af4bf919c62466f6a572f1f52f1fd24
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2023-03-26 14:09:01 +0530

    footer enabled for all routes

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,14 +1,14 @@
-import Navbar from "../navbar";
-import Footer from "../footer";
-const Layout = ({ children, withFooter = false }) => {
+import Navbar from "../navbar"
+import Footer from "../footer"
+const Layout = ({ children }) => {
   return (
     <div className="w-screen min-h-screen bg-gray-50 flex justify-center overflow-auto">
       <div className="w-full h-screen max-w-screen-2xl flex flex-col space-y-10 p-2">
         <Navbar />
         {children}
-        {withFooter && <Footer />}
+        <Footer />
       </div>
     </div>
-  );
-};
-export default Layout;
+  )
+}
+export default Layout
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,7 +4,7 @@ import Layout from "../common/Layout";
 
 const Home = () => {
   return (
-    <Layout withFooter>
+    <Layout>
       <div className="flex-grow bg-blue-100 rounded-xl py-10 px-2 md:px-5 lg:px-10">
         <AddCard />
         <hr className="my-6 border-0" />
